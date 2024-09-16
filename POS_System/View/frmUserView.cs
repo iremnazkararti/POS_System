98b139983c50e6aa5c3239d83f2c2b04ee6b8d3f
İrem Naz Karartı <iremnazkararti@gmail.com>
2024-09-16 17:50:16 +0300

    ekle butonuna kadar çalışıyor tamamen

--- POS_System/View/frmUserView.cs
+++ POS_System/View/frmUserView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POS_System.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,52 @@ namespace POS_System.View
 {
     public partial class frmUserView : SampleView
     {
+        private ListBox listBox; // ListBox'ı bir üye değişkeni olarak tanımladık
+
         public frmUserView()
         {
             InitializeComponent();
+            InitializeListBox(); // ListBox'ı form başlatılırken başlatıyoruz
         }
 
         private void frmUserView_Load(object sender, EventArgs e)
         {
+            LoadData();
+        }
 
+        public override void btnAdd_Click(object sender, EventArgs e)
+        {
+            MainClass.BlurBackground(new frmUserAdd());
+        }
+
+        public override void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            // ListBox'ı temizle
+            listBox.Items.Clear();
+
+            // Verileri ListBox'a ekle
+            listBox.Items.Add(dgvid);
+            listBox.Items.Add(dgvname);
+            listBox.Items.Add(dgvuserName);
+            listBox.Items.Add(dgvpass);
+            listBox.Items.Add(dgvphon);
+
+            string qry = @"Select userId ,uName ,uUsername ,uPass ,uPhone from users
+                            where  uName like '%" + txtSearch.Text + "%' order by userID desc";
+            MainClass.LoadData(qry, guna2DataGridView1, listBox);
+        }
+
+        private void InitializeListBox()
+        {
+            listBox = new ListBox();
+            listBox.Location = new Point(10, 10); // ListBox'ın konumunu ayarlayın
+            listBox.Size = new Size(200, 100); // ListBox'ın boyutunu ayarlayın
+            this.Controls.Add(listBox); // ListBox'ı formun kontrollerine ekleyin
         }
     }
 }
